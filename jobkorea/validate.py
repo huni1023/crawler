7388978b601ae7344d3af8d466b76128b83483bf
huni1023 <jhun1023@naver.com>
2023-04-26 08:21:44 +0900

    init: validate

---
+++ jobkorea/validate.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import re
+import argparse
+import warnings
+import collections
+import pandas as pd
+from tqdm import tqdm
+warnings.filterwarnings('ignore')
+
+# set utils
+util_path = os.pardir
+if util_path not in sys.path:
+    sys.path.append(os.pardir)
+from utils.helper import load_config
+
+# load config file
+Project_PATH = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+CONFIG_PATH = "config.yaml"
+
+# argument
+parser = argparse.ArgumentParser(description='validate')
+parser.add_argument('--solution', action='store_true',
+                    help="solution")
+parser.add_argument('--channel', action='store_true',
+                    help="channel talk")
+
+# args = parser.parse_args('') #!# in jupytor notebook case
+args = parser.parse_args()
+
+class Validator:
+    def __init__(self, file_name):
+        self.df = pd.read_excel(os.path.join(Project_PATH, 'jobkorea', 'result', file_name))
+        self.col_task2 = '사용솔루션'
+        self.col_task3 = '채널톡사용여부'
+
+    def task2(self, allowed_string):
+        r"""validation of string
+        allowed_string: list
+            list of allowed string
+        """
+        unique_val = collections.Counter(self.df[self.col_task2].unique()).keys()
+        if unique_val == allowed_string:
+            return None
+        else:
+            raise ValueError('>> something wrong, check values: ', unique_val)
+        
+
+
+
+if __name__ == '__main__':
+    validator = Validator('task2_3.xlsx')
+    
+    if args.solution:
+        validator.task2(['N', 'Y'])
+    
+    elif args.channel:
+        pass
