1fc83b0c36aaaa52f808a1b06806058461534df0
huni1023 <jhun1023@naver.com>
2023-07-14 00:54:14 +0900

    geniesnu: test list-crawling

--- geniesnu/crawling.py
+++ geniesnu/crawling.py
@@ -1,8 +1,14 @@
 import os
 import sys
+import time
 import platform
+import pandas as pd
 from pathlib import Path
+from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 # set utils
 util_path = os.pardir
@@ -15,8 +21,10 @@ from utils.telegram_bot import TelegramBot
 from utils.helper import load_config
 
 Project_PATH = Path(__file__).parent.absolute().parent.absolute()
-print('project path', Project_PATH)
 Save_PATH = os.path.join(Project_PATH, 'geniuesnu', 'result')
+
+# load .env
+load_dotenv()
 
 
 # load config file : 수정되어야 함
@@ -42,18 +50,90 @@ class snugenie_crawler(Cralwer):
     global config
     def __init__(self, url):
         if platform.system() == 'Linux':
-            print('itis: ', config['selen_path']['ubuntu2'])
             super().__init__(selenium_path=config['selen_path']['ubuntu2'],
                             chrome_option=chrome_opt)
         else:
             raise NotImplementedError
         self.url = url
         
-        # test
+        
+    def login(self, ID, PW):
+        r"""
+        Parameters
+        ----------
+        ID: str
+        PW: str
+        
+        """ 
         self.driver.get(self.url)
-        print('current url: ', self.driver.current_url)
+
+        time.sleep(2) # TBD
+
+        ID_ele= self.driver.find_element(By.XPATH, '//*[@id="login_id"]')
+        ID_ele.send_keys(ID)
+
+        PW_ele = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/fieldset/ul/li[2]/span/input')
+        PW_ele.send_keys(PW)
+
+        PW_ele.send_keys(Keys.ENTER)
+
+        assert self.driver.current_url == 'https://snugenie.snu.ac.kr/main.do'
+        print('>> login done')
+    
+
+    #!# 주관 학과, 개설 학년, 학점 구조, 교과목 영역 크롤링 필요
+    def search_word(self, word):
+        r"""
+        Parameters
+        ----------
+        word: str
+        """
+        rs = pd.DataFrame(columns='title href'.split())
+
+        search_bar_ele = self.driver.find_element(By.XPATH, '//*[@id="optSearchWord"]')
+        search_bar_ele.send_keys(word)
+        search_bar_ele.send_keys(Keys.ENTER)
+
+        time.sleep(2)
+
+        # 과목 수
+        subject_cnt_ele = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div/section[3]')
+        subject_cnt = subject_cnt_ele.find_element(By.TAG_NAME, 'strong').text
+        print('과목수: ', subject_cnt)
+
+        # 과목 크롤링
+        result_box = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div/section[4]/div[1]')
+        result_ls = result_box.find_elements(By.TAG_NAME, 'div')
+        
+        title_ls = []
+        href_ls = []
+        for one_sbj in result_ls:
+            sbj_title = one_sbj.find_element(By.CLASS_NAME, 'title').text
+            title_ls.append(sbj_title)
+
+            for idx, a_tag in enumerate(one_sbj.find_elements(By.TAG_NAME, 'a')):
+                if idx == 0 :
+                    href_ls.append(a_tag.get_attribute('href'))
+        
+        rs['title'] = title_ls
+        rs['href'] = href_ls
+        print(rs.head())
+
+        print('>> search done')
+
+        return rs
+        
+
+
+
         
 
 if __name__ == '__main__':
-    crawler = snugenie_crawler(url='https://naver.com')
+    crawler = snugenie_crawler(url='https://snugenie.snu.ac.kr')
+    crawler.login(ID= os.environ.get('mysnu-id'),
+          PW= os.environ.get('mysnu-pw'))
+    rs = crawler.search_word('개론')
+    crawler.driver.close()
+
+    rs.to_excel('./result.xlsx')
     
